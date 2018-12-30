bdddc29c4bb1fa9e1d195a84fe59f1e1a2803c84
Matthijs van Niekerk <matthijsvanniekerk@gmail.com>
2018-12-30 20:20:54 +0100

    Draw lines to the edge of the screen

--- canvas.js
+++ canvas.js
@@ -12,25 +12,43 @@
 	const lines = [];
 	var intersections = [];
 	const radius = 5;
+	const cWidth = 1000;
+	const cHeight = 1000;
 
 	function intersect(l1, l2)
 	{
 		var {start: p1, end: p2} = l1
 		var {start: p3, end: p4} = l2
 
-		var ua, ub, denom = (p4.y - p3.y)*(p2.x - p1.x) - (p4.x - p3.x)*(p2.y - p1.y);
-		if (denom == 0) {
-			return null;
-		}
-		ua = ((p4.x - p3.x)*(p1.y - p3.y) - (p4.y - p3.y)*(p1.x - p3.x))/denom;
-		ub = ((p2.x - p1.x)*(p1.y - p3.y) - (p2.y - p1.y)*(p1.x - p3.x))/denom;
-		return {
-			x: p1.x + ua * (p2.x - p1.x),
-			y: p1.y + ub * (p2.y - p1.y),
-			seg1: ua >= 0 && ua <= 1,
-			seg2: ub >= 0 && ub <= 1
-		};
+		var a1 = (p1.y - p2.y) / (p1.x - p2.x);
+		var a2 = (p3.y - p4.y) / (p3.x - p4.x);
+
+		var b1 = a1*p1.x - p1.y;
+		var b2 = a2*p3.x - p3.y;
+
+		var x = (b2 - b1)/(a1 - a2);
+		var y = a2*x + b2;
+
+		return {x, y};
 	}
+
+	function edges(p1, p2)
+	{
+		var a = (p1.y - p2.y) / (p1.x - p2.x);
+		if (a === Infinity) {
+			return {
+				start: {x: p1.x, y: 0},
+				end: {x: p1.x, y: cHeight}
+			}
+		}
+		var b = p1.y - a*p1.x;
+		var end = a*cWidth + b;
+
+		return {
+			start: {x: 0, y: b},
+			end: {x: cWidth, y: end}
+		}
+	};
 
 	canvas.addEventListener("mousemove", function (e) {
 		var rect = canvas.getBoundingClientRect();
@@ -44,10 +62,11 @@
 
 	function update() {
 		while (nodes.length > 1) {
-			let start = nodes.pop();
-			let end = nodes.pop();
+			let p1 = nodes.pop();
+			let p2 = nodes.pop();
 
-			lines.push({start: start, end: end});
+			let line = edges(p1, p2);
+			lines.push(line);
 		}
 		if (lines.length >= 2) {
 			intersections = []
@@ -62,9 +81,7 @@
 	function render() {
 		update();
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.beginPath();
-		ctx.arc(10, 10, 10, 0, 2*Math.PI);
-		ctx.fill();
+
 		for (let node of nodes) {
 			ctx.beginPath();
 			ctx.arc(node.x, node.y, radius, 0, 2*Math.PI);
